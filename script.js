255c1a71f1cc8f16010667801c6fb8f4b9faf462
Brendan Drewry <superbrendan715@yahoo.com>
2021-12-03 23:59:50 -0800

    Update script.js
    I finished the first two questions through logging it in the console.

--- script.js
+++ script.js
@@ -4,13 +4,17 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
-console.log("");
+console.log(
+  "I felt that actually walking through a lot of the material together really helped immensely in my learning process over the semester. I felt really overwhelmed when I tried to do the homework by myself, and without guidance. However, I felt that when we did go over the material together it helped a lot."
+);
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("");
+console.log(
+  "I personally would just make this course meet twice a week, instead of once a week. I felt that the time I spent in class was super essential to my learning experience in this course, and I honestly felt like I needed more time in the class. I would personally make one day a lecture and lecture activity day, and then a workshop or more in-depth lectures stretched out longer."
+);
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
